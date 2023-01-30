c21101a67d82a7c2345ecc4ad7405d7d113b5d27
Shiori <shiori.fujii.cb@gmail.com>
2023-01-30 13:09:32 -0800

    01-09 API finish weather application by express.js

---
+++ 01.The Complete 2022 Web Development Bootcamp/09.API/WeatherProject/app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+    let cityName = req.body.cityName;
+    let days = '3';
+
+    const options = {
+        "method": "GET",
+        "hostname": "weatherapi-com.p.rapidapi.com",
+        "port": null,
+        "path": "/forecast.json?q=" + cityName + "&days=" + days,
+        "headers": {
+            "X-RapidAPI-Key": "2909a03d4cmsh48c7839462aa24ep15ca6ejsn328972c49eef",
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+            "useQueryString": true
+        }
+    };
+
+    const request = https.request(options, async function (response) {
+        const chunks = [];
+                
+            response.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+                
+            response.on("end", function () {
+                const body = Buffer.concat(chunks);
+                let weatherData = JSON.parse(body.toString());
+                let condition = weatherData.current.condition.text;
+                let temperature = weatherData.current.temp_c;
+                let icon = weatherData.current.condition.icon;
+                res.write("<p>Current weather is " + condition + "<p>");
+                res.write("<img src='" + icon + "' />");
+                res.write("<h1>Current temperature in " + cityName + " is " + temperature + " degree Celcius.</h1>");
+                res.send();
+                // res.send(weatherData);
+                });
+            
+            response.on("error", () => {
+                console.log("error");
+            });
+        });
+    request.end();
+});
+
+// app.get("/", (req, res) => {
+
+//     // const url ='https://weatherapi-com.p.rapidapi.com/forecast.json';
+
+//     // https.get(url, options, (response) => {
+//     //     console.log(response.statusCode);
+//     //     response.on('data', (data) => {
+//     //         console.log(data);
+//     //         const weatherData = JSON.parse(data);
+//     //         console.log(weatherData);
+
+//     //         // const temp = weatherData.main.temp;
+//     //     });
+//     // }).on('error', (e) => {
+//     //     console.error(e);
+//     // });
+
+//     const request = https.request(options, async function (response) {
+//         const chunks = [];
+        
+//         response.on("data", function (chunk) {
+//             chunks.push(chunk);
+//         });
+        
+//         response.on("end", function () {
+//             const body = Buffer.concat(chunks);
+//             // console.log(body.toString());
+//             let weatherData = JSON.parse(body.toString());
+//             let condition = weatherData.current.condition.text;
+//             let temperature = weatherData.current.temp_c;
+//             let icon = weatherData.current.condition.icon;
+//             res.write("<p>Current weather is " + condition + "<p>");
+//             res.write("<img src='" + icon + "' />");
+//             res.write("<h1>Current temperature in Vancouver is " + temperature + " degree Celcius.</h1>");
+//             res.send();
+//             // res.send(weatherData);
+//         });
+    
+//         response.on("error", () => {
+//             console.log("error");
+//         });
+//     });
+//     request.end();
+// })
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.');
+});
